cd45eb4b381d7a81da65ca10118dd55e97bbe79b
JonathanPetit <petit.jonathan16@gmail.com>
2019-04-02 16:47:34 +0200

    basic chart temperature

--- hydroponics-app/src/Temperature.js
+++ hydroponics-app/src/Temperature.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import {Line} from 'react-chartjs-2';
 
 class Temperature extends Component {
     constructor(props) {
@@ -18,8 +19,35 @@ class Temperature extends Component {
     }
 
     render() {
+        const temperature_chart = {
+            labels: this.state.temperatures.map(temp => Date(temp.savedAt)),
+            datasets: [
+              {
+                label: 'Temperature',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: this.state.temperatures.map(temp => temp.value)
+              }
+            ]
+          };
+          
         return (
-            <div>{this.state.temperatures.map(temp => <div>{Date(temp.savedAt)}</div>)}</div>
+            <div><Line data={temperature_chart}/></div>
         )
     }
 }
